70fc87cd2650c896ddbbc54531a5e20f6620fa07
= <=>
2020-10-14 01:23:29 +0600

    terminal based dictionary

---
+++ English Dictionary/app.py
@@ -0,0 +1,35 @@
+import json
+from difflib import get_close_matches
+
+# data = json.load(open("English Dictionary/data.json"))
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    matches = get_close_matches(w,data.keys())
+
+    if w in data:
+        return data[w]
+    elif len(matches) > 0 :
+        yn =raw_input("Did you mean %s instaed?If yes,type Y otherwise N: " % matches[0])
+        if yn == "Y":
+            return data[matches[0]]
+        elif yn == "N":
+            return "The word doesn't exist.Please double check"
+        else:
+            return "We didn't understand your entry"
+    else:
+        return "The word doesn't exist. Please double check"
+
+
+word = raw_input("Enter word: ")   
+
+# print(translate(word))
+
+output = translate(word)
+
+if type(output) == list:
+    for lines in output:
+        print(lines)
+else:
+    print(output)
